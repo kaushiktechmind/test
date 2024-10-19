738e1a58cface898b9c422c5566a45af4192afd2
Kaushik <kaushik@techmindscape.com>
2024-10-19 11:22:06 +0530

    4nd commit

--- app/(admin-dashboard)/hotel/add-new-hotel/page.tsx
+++ app/(admin-dashboard)/hotel/add-new-hotel/page.tsx
@@ -14,7 +14,7 @@ import SelectUI from "@/components/SelectUI";
 import { propertyAmenities } from "@/public/data/addpropertyAmenities";
 import CheckboxCustom from "@/components/Checkbox";
 const QuillEditor = dynamic(() => import('../../../../components/QuillEditor'), { ssr: false });
-import CKEditorComponent from '../../../../components/CKEditorComponent';
+// import CKEditorComponent from '../../../../components/CKEditorComponent';
 import React, { useState } from "react";
 
 
